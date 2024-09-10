7d15982a36ef791baf3ece00a3df50bdc7a8eb2c
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-10 07:36:47 +0600

    done

--- 7/main.js
+++ 7/main.js
@@ -1,5 +1,11 @@
-const now = 2037;
-const ageFardin = now - 2003;
-const ageSiam = now - 2008;
+// const now = 2037;
+// const ageFardin = now - 2003;
+// const ageSiam = now - 2008;
 
-console.log(ageFardin > ageSiam);
+// console.log(ageFardin > ageSiam);
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Fardin"));
+console.log(Boolean({}));
+console.log(Boolean(""));

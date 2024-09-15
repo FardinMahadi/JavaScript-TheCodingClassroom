6c8105fa4d85eb6b245f1d0eb4109db021d69b72
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-15 19:53:56 +0600

    done

--- 64_GuessTheNumber/script.js
+++ 64_GuessTheNumber/script.js
@@ -10,11 +10,49 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
+const secretNumber = Math.trunc(Math.random() * 21);
+document.querySelector(".number").textContent = "?";
+
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number";
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    let highscore = score;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+    //When the guess is to high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector(".message").textContent = "📈 Too High";
+      document.querySelector(".score").textContent = score;
+    } else {
+      score--;
+      document.querySelector(".score").textContent = 0;
+      document.querySelector(".message").textContent = "💥 You Lost The Game";
+    }
+    //When the guess is to low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector(".message").textContent = "📉 Too Low";
+      document.querySelector(".score").textContent = score;
+    } else {
+      score--;
+      document.querySelector(".score").textContent = 0;
+      document.querySelector(".message").textContent = "💥 You Lost The Game";
+    }
   }
 });

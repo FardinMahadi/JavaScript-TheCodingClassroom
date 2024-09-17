4b272a8a0f998c63f50934a8f35d24b7ccc4d3ab
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-17 11:47:56 +0600

    done

--- 64_GuessTheNumber/script.js
+++ 64_GuessTheNumber/script.js
@@ -13,11 +13,11 @@
 const secretNumber = Math.trunc(Math.random() * 21);
 document.querySelector(".number").textContent = "?";
 
-let score = 20;
+let score = 20,
+  highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
@@ -26,8 +26,8 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
     document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector(".number").style.width = "30rem";
-    let highscore = score;
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
@@ -55,4 +55,10 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "💥 You Lost The Game";
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".number").textContent = "?";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".guess").value = "";
 });
